c0897849de90806a450c2b1b20f70e02b2371719
ArnaudValette <valette.arnaud.ak@gmail.com>
2023-09-26 12:13:47 +0200

    Dispatcher

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,15 @@ import React, { useEffect, useState } from "react"
 import "./App.css"
 import FloatingMenu from "./fancy-components/Floating-Menu"
 import MenuToggle from "./fancy-components/MenuToggle"
-import TreeView from "./TreeView/TreeView"
 import data from "./data/data.json"
+import TreeViewRoot from "./TreeView/TreeView"
 
 function App() {
   return (
     <div className="App">
       <MenuToggle>
         <FloatingMenu className="EffectZone" showFancyLabel>
-          <TreeView data={data} />
+          <TreeViewRoot data={data} />
         </FloatingMenu>
       </MenuToggle>
     </div>
--- src/TreeView/TreeView.tsx
+++ src/TreeView/TreeView.tsx
@@ -1,10 +1,51 @@
-import { useEffect } from "react"
+import React, { createContext, useState } from "react"
+import FloatingMenu from "../fancy-components/Floating-Menu"
 
-function TreeView(props: { data: any }) {
-  useEffect(() => {
-    console.log(props.data)
-  }, [])
-  return <div></div>
+
+function TreeViewRoot(props: { data: any, customDispatcher?: any }) {
+  const nodes:Array<any> = Object.entries(props.data)
+  const customDispatcher = props.customDispatcher || {}
+  const dispatcher = {...baseDispatcher, ...customDispatcher}
+
+  return <div>{nodes.map((entry,i)=><NodeHandler Component={dispatcher[`${entry[1].type}`]} data={entry} key={i} /> )}</div>
 }
 
-export default TreeView
+function NodeHandler({Component , data}:any){
+    const [t, sT] = useState(false)
+  function handleClick(){
+    if(data[1].children){
+      sT(true)
+    }
+  }
+  return <><div onClick={()=>handleClick()}><Component data={data[1]} />
+  </div>
+  {t ?
+   /*TODO: Absolute positionned/portaled FloatingMenu, maybe we need a component stack*/
+   <FloatingMenu className="EffectZone"><TreeViewRoot data={data[1].children} /></FloatingMenu>
+ : <></>}
+  </>
+}
+
+function HeadingNode(props:any){
+  return <h1>{props.data.display}</h1>
+}
+function ProfileNode(props:any){
+  return <></>
+}
+function FolderNode(props:any){
+  return <></>
+}
+function ImageNode(props:any){
+  return <></>
+}
+function TextNode(props:any){
+  return <></>
+}
+const baseDispatcher={
+  "heading": HeadingNode,
+  "profile": ProfileNode,
+  "image": ImageNode,
+  "folder": FolderNode,
+  "text": TextNode,
+}
+export default TreeViewRoot
