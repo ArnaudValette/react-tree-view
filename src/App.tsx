26cae077cd4e3a4a14f86c5a0a3bb91e4457a303
ArnaudValette <valette.arnaud.ak@gmail.com>
2023-09-29 15:49:27 +0200

    tree state is now a context

--- src/App.tsx
+++ src/App.tsx
@@ -4,30 +4,53 @@ import FloatingMenu from "./fancy-components/Floating-Menu"
 import MenuToggle from "./fancy-components/MenuToggle"
 import data from "./data/data.json"
 import TreeViewRoot from "./TreeView/TreeView"
+import { KeyObject } from "crypto"
 
-export const TreeStackContext = createContext<TreeStackHookType>({})
+export const TreeStackContext = createContext<TreeStackHookType>({
+  treeStack: {},
+  pushTreeStack: () => {},
+})
 
-type TreeStackHookType = {treeStack: {[key:string | number]:any}, pushTreeStack : (n:any)=>void }
+type TreeNode = any
+type WrappedNode = { [key: string | number | symbol]: TreeNode }
+type TreeStackPusher = (content: WrappedNode, level: number) => void
 
-function useTreeStack():TreeStackHookType{
-  const [ts, setTs]= useState({})
-  function pushTreeStack(n:any){
-    setTs({...ts, [n.level] : n})
+type TreeStackHookType = {
+  treeStack: WrappedNode
+  pushTreeStack: TreeStackPusher
+}
+
+function useTreeStack(): TreeStackHookType {
+  const [ts, setTs] = useState<WrappedNode>({})
+
+  const pushTreeStack: TreeStackPusher = (content, level) => {
+    /* const prev = ts[level as keyof typeof ts]
+     * if (prev && prev.id !== id) {
+     *   clearBranchWhoseRootIsAtLevel(level)
+     * } */
+    const newTree = clearBranchWhoseRootIsAtLevel(level)
+    setTs({ ...newTree, [level]: content })
   }
-  return {treeStack:ts, pushTreeStack}
+
+  const clearBranchWhoseRootIsAtLevel = (level: number) => {
+    //return Object.entries(ts).filter(([id, value]) => parseInt(id) < level)
+    const e = Object.entries(ts).filter(([key, _]) => parseInt(key) < level)
+    return Object.fromEntries(e)
+  }
+  return { treeStack: ts, pushTreeStack }
 }
 
 function App() {
-  const {treeStack, pushTreeStack} = useTreeStack()
+  const { treeStack, pushTreeStack } = useTreeStack()
   return (
-    <TreeStackContext.Provider value={{treeStack, pushTreeStack}}>
-    <div className="App">
-      <MenuToggle>
-        <FloatingMenu className="EffectZone" showFancyLabel>
-          <TreeViewRoot data={data} />
-        </FloatingMenu>
-      </MenuToggle>
-    </div>
+    <TreeStackContext.Provider value={{ treeStack, pushTreeStack }}>
+      <div className="App">
+        <MenuToggle>
+          <FloatingMenu className="EffectZone" showFancyLabel>
+            <TreeViewRoot data={data} />
+          </FloatingMenu>
+        </MenuToggle>
+      </div>
     </TreeStackContext.Provider>
   )
 }
--- src/TreeView/TreeView.tsx
+++ src/TreeView/TreeView.tsx
@@ -1,61 +1,97 @@
-import React, { PropsWithChildren, ReactChildren, ReactPropTypes, createContext, useContext, useReducer, useState } from "react"
+import React, {
+  PropsWithChildren,
+  ReactChildren,
+  ReactPropTypes,
+  createContext,
+  useContext,
+  useReducer,
+  useState,
+} from "react"
 import FloatingMenu from "../fancy-components/Floating-Menu"
 import { createPortal } from "react-dom"
 import { TreeStackContext } from "../App"
 
-const x = createContext({})
-
-function TreeViewRoot(props: { data: any, customDispatcher?: any, level?:number }) {
-  const nodes:Array<any> = Object.entries(props.data)
-    const { treeStack, pushTreeStack } = useContext(TreeStackContext)
+function TreeViewRoot(props: {
+  data: any
+  customDispatcher?: any
+  level?: number
+}) {
+  const nodes: Array<any> = Object.entries(props.data)
+  const { treeStack, pushTreeStack } = useContext(TreeStackContext)
   const level = props.level || 0
   const customDispatcher = props.customDispatcher || {}
-  const dispatcher = {...baseDispatcher, ...customDispatcher}
+  const dispatcher = { ...baseDispatcher, ...customDispatcher }
 
-  return <div>{nodes.map((entry,i)=><NodeHandler level={level+1} Component={dispatcher[`${entry[1].type}`]} data={entry} key={i} /> )}</div>
+  return (
+    <>
+      <div>
+        {nodes.map((entry, i) => (
+          <NodeHandler
+            pusher={pushTreeStack}
+            level={level + 1}
+            Component={dispatcher[`${entry[1].type}`]}
+            data={entry}
+            key={i}
+          />
+        ))}
+      </div>
+      {treeStack[level + 1] ? (
+        <Portal>
+          <FloatingMenu className="EffectZone">
+            <TreeViewRoot level={level + 1} data={treeStack[level + 1]} />
+          </FloatingMenu>
+        </Portal>
+      ) : (
+        <></>
+      )}
+    </>
+  )
 }
 
-function NodeHandler({Component , data, level}:any){
-    const [t, sT] = useState(false)
-  function handleClick(){
-    if(data[1].children){
-      sT(true)
+function NodeHandler({ Component, data, level, pusher }: any) {
+  function handleClick() {
+    if (data[1].children) {
+      pusher(data[1].children, level)
     }
   }
-  return <><div onClick={()=>handleClick()}><Component data={data[1]} />
-  </div>
-  {t ?
-   /*TODO: Absolute positionned/portaled FloatingMenu, maybe we need a component stack*/
-   <Portal><FloatingMenu className="EffectZone"><TreeViewRoot level={level} data={data[1].children} /></FloatingMenu></Portal>
- : <></>}
-  </>
+  return (
+    <>
+      <div onClick={() => handleClick()}>
+        <Component data={data[1]} />
+      </div>
+    </>
+  )
 }
 
-function Portal(props:PropsWithChildren){
-    const element: Element | null = document.getElementById("portalFrame")
-    return element ? createPortal(<div className="portal">{props.children}</div>, element ):<></>
+function Portal(props: PropsWithChildren) {
+  const element: Element | null = document.getElementById("portalFrame")
+  return element ? (
+    createPortal(<div className="portal">{props.children}</div>, element)
+  ) : (
+    <></>
+  )
 }
 
-function HeadingNode(props:any){
+function HeadingNode(props: any) {
   return <h1>{props.data.display}</h1>
 }
-function ProfileNode(props:any){
+function ProfileNode(props: any) {
   return <></>
 }
-function FolderNode(props:any){
+function FolderNode(props: any) {
   return <></>
 }
-function ImageNode(props:any){
+function ImageNode(props: any) {
   return <></>
 }
-function TextNode(props:any){
+function TextNode(props: any) {
   return <></>
 }
-const baseDispatcher={
-  "heading": HeadingNode,
-  "profile": ProfileNode,
-  "image": ImageNode,
-  "folder": FolderNode,
-  "text": TextNode,
+const baseDispatcher = {
+  heading: HeadingNode,
+  profile: ProfileNode,
+  image: ImageNode,
+  folder: FolderNode,
+  text: TextNode,
 }
 export default TreeViewRoot
